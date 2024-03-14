f64c1ae5bc476c0599528fc74763b32e9bf8d334
sunkyoung <yskkjdh@gmail.com>
2024-03-14 12:31:15 +0900

    refactor : vaildation 어노테이션 추가
    Change-Id: I9cb9e004fe575b244b008bf6ffb2609c966adf25

--- bada-back/src/main/java/com/bada/badaback/auth/dto/AuthJoinRequestDto.java
+++ bada-back/src/main/java/com/bada/badaback/auth/dto/AuthJoinRequestDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c718a4056b6acc402e2759ab54a2f4c36dd94b6490bb4ec371dbf6fbfef186f9
-size 238
+oid sha256:c58adc665b5f050d4ee809a71140ead55d4ec19a106fa09716136c8f35e5e763
+size 660
--- bada-back/src/main/java/com/bada/badaback/auth/dto/AuthSignUpRequestDto.java
+++ bada-back/src/main/java/com/bada/badaback/auth/dto/AuthSignUpRequestDto.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:917f07f56ba237e47ec9ff60d296c6ff58e521dc2d0d91b87a9c99e947489be9
-size 242
+oid sha256:9b0ab85f8b8da7ecc546787e686b8a3f1c986f595f9142fe1d7022cfb3053862
+size 669

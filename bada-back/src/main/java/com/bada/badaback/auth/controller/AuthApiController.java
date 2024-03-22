67da0d09bdae3519bf0811b262f8a85cb45432ae
sunkyoung <yskkjdh@gmail.com>
2024-03-22 15:56:32 +0900

    refactor : 유효기간 오류 메시지 변경
    Change-Id: Ica82b57b9f28fbfd2998f58da0092003320015da

--- bada-back/src/main/java/com/bada/badaback/auth/controller/AuthApiController.java
+++ bada-back/src/main/java/com/bada/badaback/auth/controller/AuthApiController.java
@@ -1,3 +1,65 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:98c31f38271c66579c0d10698c08eab3e6e4b457d7fe7d0aeb2aa639553dd960
-size 2885
+package com.bada.badaback.auth.controller;
+
+import com.bada.badaback.auth.dto.*;
+import com.bada.badaback.auth.service.AuthCodeService;
+import com.bada.badaback.auth.service.AuthService;
+import com.bada.badaback.global.annotation.ExtractPayload;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "Auth", description = "AuthApiController")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthApiController {
+    private final AuthService authService;
+    private final AuthCodeService authCodeService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<LoginResponseDto> signup(@RequestBody @Valid AuthSignUpRequestDto requestDto) {
+        Long memberId = authService.signup(requestDto.name(), requestDto.phone(), requestDto.email(),
+                requestDto.social(), requestDto.profileUrl(), requestDto.familyName(), requestDto.fcmToken());
+        LoginResponseDto responseDto = authService.login(memberId);
+        authCodeService.issueCode(memberId);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @PostMapping("/join")
+    public ResponseEntity<LoginResponseDto> join(@RequestBody @Valid AuthJoinRequestDto requestDto) {
+        Long memberId = authService.join(requestDto.name(), requestDto.phone(), requestDto.email(),
+                requestDto.social(), requestDto.profileUrl(), requestDto.code(), requestDto.fcmToken());
+        LoginResponseDto responseDto = authService.login(memberId);
+        authCodeService.issueCode(memberId);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @PostMapping("/joinChild")
+    public ResponseEntity<LoginResponseDto> joinChild(@RequestBody @Valid AuthJoinChildRequestDto requestDto) {
+        Long memberId = authService.joinChild(requestDto.name(), requestDto.phone(), requestDto.profileUrl(), requestDto.code(),
+                requestDto.fcmToken());
+        LoginResponseDto responseDto = authService.login(memberId);
+        authCodeService.issueCode(memberId);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<LoginResponseDto> alreadyMember(@RequestBody @Valid AuthAlreadyRequestDto requestDto) {
+        Long memberId = authService.AlreadyMember(requestDto.email(), requestDto.social());
+        if(memberId == null){
+            return ResponseEntity.noContent().build();
+        }
+        LoginResponseDto responseDto = authService.login(memberId);
+        authCodeService.issueCode(memberId);
+        return ResponseEntity.ok(responseDto);
+    }
+
+    @GetMapping("/logout")
+    public ResponseEntity<Void> logout(@ExtractPayload Long memberId) {
+        authService.logout(memberId);
+        return ResponseEntity.ok().build();
+    }
+}
+

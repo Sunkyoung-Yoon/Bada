212550306f798771ae97597851c3af12fc78fd94
이창헌 <changbill@naver.com>
2024-03-25 13:52:50 +0900

    feat : myplace 일부 수정 및 아이 앱 구현
    Change-Id: I0d5a2b75b5eb4a8a497460bc808a385edc860471

---
+++ bada-front/lib/screens/main/path_recommend/searching_path.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7e9d49a48b907c649652eb06a182218aeee771b93a53b677a93d9d4f45417d0a
+size 408
---
+++ bada_kids_front/android/app/src/main/kotlin/com/bada/kids/MainActivity.kt
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d4ef96094d1046c537df01152875c7d761092acfcb305288023df2c7334bc966
+size 114

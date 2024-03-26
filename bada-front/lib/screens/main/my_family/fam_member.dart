3fb16e5889d679f66558c2782798577b54c3b350
정휘원 <wonnyboi@naver.com>
2024-03-26 10:08:01 +0900

    feat: 추가 안되는 장소 변경 완료, 디테일 페이지 입장 시 지도 안뜸

--- bada-front/lib/screens/main/my_family/fam_member.dart
+++ bada-front/lib/screens/main/my_family/fam_member.dart
@@ -1,3 +1,131 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:1b7dd224aa8add705ec8839686172b293297dd287f55e242ebe98cbaea6a2eaf
-size 4141
+import 'package:bada/screens/main/my_family/alarm_list.dart';
+import 'package:bada/screens/main/my_family/child_setting.dart';
+import 'package:bada/widgets/buttons.dart';
+import 'package:bada/widgets/screensize.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+class FamilyMember extends StatefulWidget {
+  final String name;
+  final String? profileUrl;
+  final int isParent;
+  final int? movingState;
+  const FamilyMember({
+    super.key,
+    required this.name,
+    required this.isParent,
+    this.profileUrl,
+    this.movingState,
+  });
+
+  @override
+  State<FamilyMember> createState() => _FamilyMemberState();
+}
+
+class _FamilyMemberState extends State<FamilyMember> {
+  String myName = '';
+
+  @override
+  void initState() {
+    super.initState();
+    loadMyName();
+  }
+
+  void loadMyName() async {
+    const storage = FlutterSecureStorage();
+    String? name = await storage.read(key: 'nickname');
+    if (name != null) {
+      setState(() {
+        myName = name;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        padding: const EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          border: Border.all(color: const Color(0xff696DFF), width: 0.5),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        child: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  backgroundColor: Colors.black,
+                  radius: 35,
+                  backgroundImage: widget.profileUrl != null
+                      ? NetworkImage(widget.profileUrl!)
+                      : null,
+                  child: widget.profileUrl == null
+                      ? const Icon(
+                          Icons.person,
+                          size: 50,
+                          color: Colors.white,
+                        ) // Example: default icon
+                      : null,
+                ),
+                SizedBox(width: UIhelper.scaleWidth(context) * 10),
+                // Text(
+                //   '이름 : ${widget.name}',
+                //   style: const TextStyle(fontSize: 16),
+                // ),
+                if (widget.name == myName)
+                  const Text(
+                    '나!',
+                    style: TextStyle(fontSize: 20),
+                  )
+                else
+                  Text(
+                    '이름 : ${widget.name}',
+                    style: const TextStyle(fontSize: 16),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                SizedBox(width: UIhelper.scaleWidth(context) * 10),
+                if (widget.isParent == 0) ...[
+                  Button281_77(
+                    label: const Text('알림 기록'),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => AlarmList(name: widget.name),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ],
+            ),
+            if (widget.isParent == 0) ...[
+              SizedBox(height: UIhelper.scaleHeight(context) * 10),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Button281_77(label: Text('정지 중')),
+                  Button281_77(
+                    label: const Text('설정'),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) =>
+                              ChildeSetting(name: widget.name),
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
+            ],
+          ],
+        ),
+      ),
+    );
+  }
+}

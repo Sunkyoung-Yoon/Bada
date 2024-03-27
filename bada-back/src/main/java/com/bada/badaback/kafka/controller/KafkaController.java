8bef7318cf21784635fc39dcfdbc05aeb7ab63e4
이용준 <sark2@naver.com>
2024-03-27 15:32:25 +0900

    edit: AlarmLog 엔티티 수정 및 알림 기록 조회 변경
    Change-Id: I736f68c804d5da9f5419e5af5f576d30b61a2904

--- bada-back/src/main/java/com/bada/badaback/kafka/controller/KafkaController.java
+++ bada-back/src/main/java/com/bada/badaback/kafka/controller/KafkaController.java
@@ -1,3 +1,29 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:31da9855cb65dc997358f57baffa8b817041423c874e3aa83695e8af700b5b6d
-size 987
+package com.bada.badaback.kafka.controller;
+
+import com.bada.badaback.kafka.dto.AlarmDto;
+import com.bada.badaback.kafka.service.KafkaProducerService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/kafka")
+@Slf4j
+@RequiredArgsConstructor
+public class KafkaController {
+
+  private final KafkaProducerService kafkaProducerService;
+
+  @PostMapping("/alarm")  // alarm 이용
+  @ResponseBody
+  public String sendAlarm(@RequestBody AlarmDto alarmDto) {
+    log.info("################## send alarm - AlarmDto : {}", alarmDto.toString());
+
+    kafkaProducerService.sendAlarm(alarmDto);
+    return "success";
+  }
+}

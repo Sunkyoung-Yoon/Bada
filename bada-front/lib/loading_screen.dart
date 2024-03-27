25c5d18819069a54929f0452bb9ffdd7552b8cfe
이창헌 <changbill@naver.com>
2024-03-27 10:32:07 +0900

    feat : 마이플레이스 Json parsing 이슈 해결 및 login 이슈 해결
    Change-Id: I1be31a5466d36acd13a6db06bee18638f840c4f5

---
+++ bada-front/lib/loading_screen.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:210af08b89e66425ea86e93d1165fd6e077446cb3fd5cd5efe67be7b473dbf06
+size 3655
---
+++ bada-front/lib/screens/main/my_place/my_place.dart
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:3db51bdd4a4db56fccfbb4c7202195d81e0e080ab57301353a11a380228172f5
+size 4857

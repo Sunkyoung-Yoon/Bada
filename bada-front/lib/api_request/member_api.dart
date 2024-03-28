c077f3ff566e6ed00771d37585063a9b5e5fa14d
이창헌 <changbill@naver.com>
2024-03-28 17:48:19 +0900

    feat : member_api 파일 수정
    Change-Id: Ib9ea5d425b231e0b728dab990504c40d740cd093

--- bada-front/lib/api_request/member_api.dart
+++ bada-front/lib/api_request/member_api.dart
@@ -1,33 +1,3 @@
-import 'package:http/http.dart' as http;
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
-
-class MembersApi {
-  static const String _baseUrl = 'https://j10b207.p.ssafy.io/api/members';
-  final FlutterSecureStorage _storage = const FlutterSecureStorage();
-
-  Future<void> deleteMember() async {
-    try {
-      String? accessToken = await _storage.read(key: 'accessToken');
-
-      if (accessToken == null) {
-        throw Exception('Access token not found');
-      }
-
-      Map<String, String> headers = {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $accessToken',
-      };
-
-      final response = await http.delete(Uri.parse(_baseUrl), headers: headers);
-
-      if (response.statusCode == 200) {
-        print('Member deleted successfully');
-      } else {
-        throw Exception('Failed to delete member');
-      }
-    } catch (e) {
-      print(e.toString());
-      throw Exception('Error deleting member');
-    }
-  }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:2c84ed1c384cdc404c7717b1dea266687da28ecbee1bfd8ada170455eda2bb34
+size 2631

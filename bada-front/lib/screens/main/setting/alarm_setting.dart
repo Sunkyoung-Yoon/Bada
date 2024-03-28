dd3e20bcf3174c759d72c8b0d2be30e1af203dec
정휘원 <wonnyboi@naver.com>
2024-03-28 14:05:24 +0900

    testing alarm

--- bada-front/lib/screens/main/setting/alarm_setting.dart
+++ bada-front/lib/screens/main/setting/alarm_setting.dart
@@ -1,29 +1,3 @@
-import 'package:flutter/material.dart';
-import 'dart:ui';
-
-class AlarmSetting extends StatelessWidget {
-  const AlarmSetting({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('알람 설정'),
-        centerTitle: true,
-        backgroundColor: Colors.white,
-      ),
-      body: Column(
-        children: <Widget>[
-          Text('displaySize : ${MediaQuery.of(context).size}'),
-          Text('displayHeight : ${MediaQuery.of(context).size.height}'),
-          Text('displayWidth : ${MediaQuery.of(context).size.width}'),
-          Text(
-            'devicePixelRatio : ${MediaQuery.of(context).devicePixelRatio}',
-          ),
-          Text('statusBarHeight : ${MediaQuery.of(context).padding.top}'),
-          Text('window.physicalSize : ${window.physicalSize}'),
-        ],
-      ),
-    );
-  }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:1dee0fcd47304fe44feca2ee68a6a839d4dd9bbb62661dbbf8ed09e37a90fcd9
+size 887

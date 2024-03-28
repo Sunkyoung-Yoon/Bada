efff2ebbe67e2ecfa2c5851c24f7df7df3f5682e
정휘원 <wonnyboi@naver.com>
2024-03-28 17:28:43 +0900

    feat: FCM 알람 수신 세팅 성공

--- bada-front/lib/main.dart
+++ bada-front/lib/main.dart
@@ -1,3 +1,130 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:d0a9ca473817fe9bf62e58a3c277f243780ad687a6b92e29c93a1da5a45276dd
-size 2681
+import 'package:bada/models/screen_size.dart';
+import 'package:bada/provider/profile_provider.dart';
+import 'package:bada/screens/main/main_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:kakao_flutter_sdk/kakao_flutter_sdk.dart';
+import 'package:kakao_flutter_sdk_common/kakao_flutter_sdk_common.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
+import 'package:bada/screens/login/login_screen.dart';
+import 'package:bada/loading_screen.dart';
+import 'package:kakao_map_plugin/kakao_map_plugin.dart';
+import 'package:provider/provider.dart';
+import 'firebase_options.dart';
+
+@pragma('vm:entry-point')
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // await Firebase.initializeApp();
+  print("Handling a background message: ${message.messageId}");
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  await dotenv.load(fileName: '.env');
+
+  AuthRepository.initialize(appKey: dotenv.env['KAKAO_MAP_API'] ?? '');
+
+  KakaoSdk.init(
+    nativeAppKey: '9d4c295f031b5c1f50269e353e895e12',
+  );
+
+  requestNotificationPermission();
+
+  await FirebaseMessaging.instance.setAutoInitEnabled(true);
+
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
+
+  final fcmToken = await FirebaseMessaging.instance.getToken();
+  print('fcmtoken main.dart 56 $fcmToken');
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ProfileProvider()),
+        Provider<ScreenSizeModel>(
+          create: (_) => ScreenSizeModel(
+            screenWidth: 0,
+            screenHeight: 0,
+          ),
+          lazy: false,
+        ),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+Future<void> requestNotificationPermission() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final FlutterSecureStorage _storage = const FlutterSecureStorage();
+
+  Future<bool> initializeApp() async {
+    await Future.delayed(const Duration(seconds: 2));
+    String? accessToken = await _storage.read(key: 'accessToken');
+    return accessToken != null;
+  }
+  //로그인 시, 단순 storage reading이 아니라,
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(fontFamily: 'Pretendard'),
+      home: const LoadingScreen(),
+      // FutureBuilder<bool>(
+      //   future: initializeApp(),
+      //   builder: (context, snapshot) {
+      //     if (snapshot.connectionState == ConnectionState.done) {
+      //       if (snapshot.data == true) {
+      //         return const HomeScreen();
+      //       } else {
+      //         return const LoginScreen();
+      //       }
+      //     } else {
+      //       return const LoadingScreen();
+      //     }
+      //   },
+      // ),
+    );
+  }
+}
--- bada-front/lib/screens/main/path_recommend/screen/path_map.dart
+++ bada-front/lib/screens/main/path_recommend/screen/path_map.dart
@@ -1,3 +1,47 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a5d4f7e3ac8057fb6ed0325a1dd383a2c93abc4a350d1a882f1d5d1df659443c
-size 1403
+import 'package:flutter/material.dart';
+import 'package:kakao_map_plugin/kakao_map_plugin.dart';
+
+class PathMap extends StatefulWidget {
+  // TODO : 경로 List 받아오기 및 경로 폴리라인 그리기
+  const PathMap({super.key});
+
+  @override
+  State<PathMap> createState() => _PathMapState();
+}
+
+class _PathMapState extends State<PathMap> {
+  late KakaoMapController mapController;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('경로 추천'),
+      ),
+      body: Stack(
+        // TODO : 아이의 정보를 받아오기 POST요청 및 그 숫자만큼 CircleAvatar 생성
+        // TODO : circleAvatar를 터치하면 아이의 위치를 표시할 수 있도록 함
+        children: [
+          KakaoMap(
+            onMapCreated: (controller) {
+              mapController = controller;
+            },
+          ),
+          Positioned(
+            right: 20, // 오른쪽으로부터 20px 떨어진 위치
+            top: 20, // 상단으로부터 20px 떨어진 위치
+            child: GestureDetector(
+              onTap: () {
+                debugPrint("CircleAvatar 터치!");
+              },
+              child: const CircleAvatar(
+                backgroundColor: Colors.blue,
+                child: Icon(Icons.person),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}

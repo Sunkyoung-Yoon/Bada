5108b2e1400775910a7b895c1c8acc06c66f1392
sunkyoung <yskkjdh@gmail.com>
2024-03-28 15:49:22 +0900

    feat : updateMovingState() 0->1, 1->0이 아니라 현재 위치 생성하면 1로, 삭제할 때는 0으로 무조건 변경하도록 수정

--- bada-back/src/main/java/com/bada/badaback/alarmlog/domain/AlarmLog.java
+++ bada-back/src/main/java/com/bada/badaback/alarmlog/domain/AlarmLog.java
@@ -1,3 +1,51 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:4dde14198549c0ee9919d2a749f65bfa72bc1ce25f650f52bd877a6b2340742e
-size 1311
+package com.bada.badaback.alarmlog.domain;
+
+
+import com.bada.badaback.global.BaseTimeEntity;
+import com.bada.badaback.member.domain.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@Table(name = "alarm_log")
+public class AlarmLog extends BaseTimeEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "alarm_id")
+  private Long id;
+
+
+  @Column(nullable = false)
+  private String type;
+
+  @Column(name = "child_id", nullable = false)
+  private Long childId;
+
+
+  @ManyToOne(fetch = FetchType.LAZY)
+  @JoinColumn(name = "member_id", referencedColumnName = "member_id", nullable = false)
+  private Member member;
+
+  private AlarmLog(String type, Member member, Long childId) {
+    this.type = type;
+    this.member = member;
+    this.childId = childId;
+  }
+
+  public static AlarmLog createAlarmLog(String type, Member member, Long childId) {
+    return new AlarmLog(type, member, childId);
+  }
+
+}
--- bada-back/src/main/java/com/bada/badaback/currentLocation/controller/CurrentLocationApiController.java
+++ bada-back/src/main/java/com/bada/badaback/currentLocation/controller/CurrentLocationApiController.java
@@ -1,3 +1,39 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8546616d88dd82c3b1165216493c2286ac390e2462ca23bb8c03491cfc374114
-size 1855
+package com.bada.badaback.currentLocation.controller;
+
+import com.bada.badaback.currentLocation.dto.CurrentLocationRequestDto;
+import com.bada.badaback.currentLocation.dto.CurrentLocationResponseDto;
+import com.bada.badaback.currentLocation.service.CurrentLocationService;
+import com.bada.badaback.global.annotation.ExtractPayload;
+import com.bada.badaback.member.service.MemberService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@Tag(name = "최근 위치", description = "CurrentLocationApiController")
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/currentLocation")
+public class CurrentLocationApiController {
+    private final CurrentLocationService currentLocationService;
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity<Void> create(@ExtractPayload Long memberId, @RequestBody @Valid CurrentLocationRequestDto requestDto) {
+        currentLocationService.create(memberId, requestDto.currentLatitude(), requestDto.currentLongitude());
+        memberService.updateMovingState(memberId, 1);
+        return ResponseEntity.ok().build();
+    }
+
+    @PatchMapping
+    public ResponseEntity<Void> update(@ExtractPayload Long memberId, @RequestBody @Valid CurrentLocationRequestDto requestDto) {
+        currentLocationService.update(memberId, requestDto.currentLatitude(), requestDto.currentLongitude());
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/{childId}")
+    public ResponseEntity<CurrentLocationResponseDto> readCurrentLocation(@ExtractPayload Long memberId, @PathVariable("childId") Long childId) {
+        return ResponseEntity.ok(currentLocationService.read(memberId, childId));
+    }
+}

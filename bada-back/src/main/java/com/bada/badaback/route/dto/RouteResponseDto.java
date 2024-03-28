3c63c9328d0d49117f6486c9ee03bcb702062384
SIMJIYEON <sim.yyeon@gmail.com>
2024-03-28 11:15:41 +0900

    feat: 경로를 포함한 일정 범위 확인 메서드 생성

--- bada-back/src/main/java/com/bada/badaback/route/dto/RouteResponseDto.java
+++ bada-back/src/main/java/com/bada/badaback/route/dto/RouteResponseDto.java
@@ -1,25 +1,3 @@
-package com.bada.badaback.route.dto;
-
-import com.bada.badaback.route.domain.Route;
-import com.bada.badaback.safefacility.domain.Point;
-import lombok.Builder;
-
-import java.util.List;
-
-@Builder
-public record RouteResponseDto(
-        double startLng,
-        double startLat,
-        double endLng,
-        double endLat,
-        List<Point> pointList
-) {
-    public static RouteResponseDto from(Route route, List<Point> pointList) {
-        return RouteResponseDto.builder().startLng(Double.parseDouble(route.getStartLongitude()))
-                .startLat(Double.parseDouble(route.getStartLatitude()))
-                .endLng(Double.parseDouble(route.getEndLongitude()))
-                .endLat(Double.parseDouble(route.getEndLatitude()))
-                .pointList(pointList)
-                .build();
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:822b0f247eb96b479ae54959fd2486454bf87ee4109dbd9fb3c3cb961e2991c9
+size 811

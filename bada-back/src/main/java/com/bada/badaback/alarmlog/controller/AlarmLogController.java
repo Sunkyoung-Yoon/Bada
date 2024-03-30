0c1668efcf343f04eb63b640a5c164608a397404
sunkyoung <yskkjdh@gmail.com>
2024-03-30 23:59:02 +0900

    refactor : 회원(부모,아이) 정보 수정 API로 넘어온 데이터를 @RequestParam으로 받도록 수정
    Change-Id: Ic8e7782c7b4cbe5efaaa1acea6db412ef91ccdba

--- bada-back/src/main/java/com/bada/badaback/alarmlog/controller/AlarmLogController.java
+++ bada-back/src/main/java/com/bada/badaback/alarmlog/controller/AlarmLogController.java
@@ -1,3 +1,41 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:37ac43c2f9516aab856ee765701c4b46fc6b2d2bcd28a7b442fdc071e9986a57
-size 1463
+package com.bada.badaback.alarmlog.controller;
+
+import com.bada.badaback.alarmlog.dto.AlarmLogResponseDto;
+import com.bada.badaback.alarmlog.service.AlarmLogService;
+import com.bada.badaback.global.annotation.ExtractPayload;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/api/alarmLog/list")
+@Slf4j
+@RequiredArgsConstructor
+public class AlarmLogController {
+
+  private final AlarmLogService alarmLogService;
+
+  // 아이 알림 기록 조회
+  @GetMapping("/{childId}")  // alarm 이용
+  @ResponseBody
+  public ResponseEntity<List<AlarmLogResponseDto>> getAlarmLogsByMemberIdAndChildId(@ExtractPayload Long memberId, @PathVariable Long childId) {
+    return ResponseEntity.ok().body(alarmLogService.getAlarmLogsByMemberIdAndChildId(memberId, childId));
+  }
+
+  // 안읽은 아이 알림 개수 조회
+
+
+
+  // 알림 읽음 처리 - 동시에 쿼리가 진행되어야하지않나? - 무조건 동시에 처리되어야한다. 하나의 API요청으로 조회하면서 가져오고 쿼리가 나가야한다.
+
+
+
+
+
+}

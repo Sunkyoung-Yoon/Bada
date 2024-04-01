8d99cfeac18bd852ebbd4f0a24a2f066488da33f
sunkyoung <yskkjdh@gmail.com>
2024-04-01 13:09:46 +0900

    feat : 회원가입 시 기존회원이라면 updateFcmToken() 호출
    Change-Id: Ieec6a00e6c5d56f5f834ccf34ecee926911a6ed5

--- bada-back/src/main/java/com/bada/badaback/route/dto/RouteResponseDto.java
+++ bada-back/src/main/java/com/bada/badaback/route/dto/RouteResponseDto.java
@@ -1,25 +1,3 @@
-package com.bada.badaback.route.dto;
-
-import com.bada.badaback.route.domain.Route;
-import com.bada.badaback.safefacility.domain.Point;
-import lombok.Builder;
-
-import java.util.List;
-
-@Builder
-public record RouteResponseDto(
-        double startLng,
-        double startLat,
-        double endLng,
-        double endLat,
-        List<Point> pointList
-) {
-    public static RouteResponseDto from(double startLat, double startLng, double endLat, double endLng, List<Point> pointList) {
-        return RouteResponseDto.builder().startLng(startLng)
-                .startLat(startLat)
-                .endLng(endLng)
-                .endLat(endLat)
-                .pointList(pointList)
-                .build();
-    }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:3400ea7abbfe750549b2e6143f6f4f0784c4bbc186c0eed29879165b4ca234ab
+size 716

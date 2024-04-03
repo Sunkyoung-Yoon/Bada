098d78d306a4f064024ea4e2c1a6faff3a5530d8
이창헌 <changbill@naver.com>
2024-04-03 22:18:28 +0900

    feat : 우리 가족 현재 위치 구현
    Change-Id: Ifa9d3a3736834ace1830da6a6b1327b493108d2d

--- bada-front/lib/screens/main/my_family/model/current_location.dart
+++ bada-front/lib/screens/main/my_family/model/current_location.dart
@@ -1,25 +1,3 @@
-class CurrentLocation {
-  final double currentLatitude;
-  final double currentLongitude;
-  final int childId;
-  final String childName;
-  final String? childProfileUrl;
-
-  CurrentLocation({
-    required this.currentLatitude,
-    required this.currentLongitude,
-    required this.childId,
-    required this.childName,
-    required this.childProfileUrl,
-  });
-
-  factory CurrentLocation.fromJson(Map<String, dynamic> json) {
-    return CurrentLocation(
-      currentLatitude: json['currentLatitude'],
-      currentLongitude: json['currentLongitude'],
-      childId: json['childId'],
-      childName: json['childName'],
-      childProfileUrl: json['childProfileUrl'],
-    );
-  }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:cf86278957b12ff827e66ab0995b1fb50209eedf7a0da013a2ad8a67cf240aa0
+size 637
